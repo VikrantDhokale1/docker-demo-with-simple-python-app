d772e0b4ea6379617c0d84574485e020ed6f401d
VikrantDhokale1 <139742612+VikrantDhokale1@users.noreply.github.com>
2023-09-11 13:22:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,4 @@ def hello():
     """Return a friendly HTTP greeting."""
     return 'Hello World'
 
-@app.errorhandler(500)
-def server_error(e):
-    logging.exception('An error occurred during a request.')
-    return """
-    An internal error occurred: <pre>{}</pre>
-    See logs for full stacktrace.
-    """.format(e), 500
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
